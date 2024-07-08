ac6e769e0db7db91240e743a8f5078cbb9800edb
om50 <omchy34@gmail.com>
2024-07-08 23:43:03 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN ,
     methods: "POST , GET , DELETE , PATCH , HEAD",
     credentials: true,
   })
