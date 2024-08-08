300b3105fe0ea6d31d118f55f26dc7d54268482e
Om Choudhary <76719323+omchy34@users.noreply.github.com>
2024-08-08 16:32:40 +0000

    yui

--- src/middlewere/Auth.middlewere.js
+++ src/middlewere/Auth.middlewere.js
@@ -48,10 +48,10 @@ export const verifyAdminJWT = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decoded._id);
-    if (!user || !user.isAdmin) {
+    if (!user.isAdmin) {
       return next(new ApiError(403, "Admin access required."));
     }
-    req.user = user;
+
     next();
   } catch (err) {
     next(new ApiError(401, "Invalid token."));
